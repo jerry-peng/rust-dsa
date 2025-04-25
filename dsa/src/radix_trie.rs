b165b28cd6d94cdbaa929da621b4d90b6f3d6680
Jerry Peng <jerrypeng0112@hotmail.com>
2025-04-25 00:05:41 -0700

    Finished radix tree, just need to add comments/doc tests

--- dsa/src/radix_trie.rs
+++ dsa/src/radix_trie.rs
@@ -271,7 +271,12 @@ impl RadixTree {
             let status = Node::cmp_chunk(&mut word_iter, chunk);
 
             match status {
-                ChunkCmpStatus::Identical | ChunkCmpStatus::WordInChunk { .. } => {
+                ChunkCmpStatus::Identical => {
+                    // if word is empty and tree is empty, it matches identical but there should be
+                    // no string that matches prefix
+                    return !self.is_empty();
+                }
+                ChunkCmpStatus::WordInChunk { .. } => {
                     return true;
                 }
                 ChunkCmpStatus::Mismatch { .. } => {
@@ -284,6 +289,67 @@ impl RadixTree {
             }
         }
     }
+
+    pub fn iter(&self) -> Iter {
+        self.iter_prefix("")
+    }
+
+    pub fn iter_prefix(&self, prefix: &str) -> Iter {
+        let mut curr_node = &self.root;
+        let mut word_iter = prefix.bytes();
+
+        let tail_trunc_len = loop {
+            let chunk = &curr_node.chunk;
+            let status = Node::cmp_chunk(&mut word_iter, chunk);
+
+            match status {
+                ChunkCmpStatus::Identical => {
+                    // len still include node key
+                    let tail_trunc_len = curr_node.chunk.len();
+                    break tail_trunc_len;
+                }
+                ChunkCmpStatus::WordInChunk {
+                    key: _,
+                    chunk_remain,
+                } => {
+                    // len still include node key, -1 is for accounting key being consumed from chunk iter
+                    let tail_trunc_len = curr_node.chunk.len() - chunk_remain.len() - 1;
+                    break tail_trunc_len;
+                }
+                ChunkCmpStatus::Mismatch { .. } => {
+                    return Iter {
+                        prefix: Vec::new(),
+                        nodes: Vec::new(),
+                    };
+                }
+                ChunkCmpStatus::ChunkInWord { key } => match curr_node.nodes.get(&key) {
+                    Some(node) => curr_node = node,
+                    None => {
+                        return Iter {
+                            prefix: Vec::new(),
+                            nodes: Vec::new(),
+                        };
+                    }
+                },
+            }
+        };
+
+        // truncate to parent prefix byte len
+        let mut prefix_bytes = prefix.as_bytes().to_vec();
+        prefix_bytes.truncate(prefix_bytes.len() - tail_trunc_len);
+
+        let key = prefix_bytes.pop();
+        let parent_prefix_len = prefix_bytes.len();
+
+        Iter {
+            prefix: prefix_bytes,
+            nodes: vec![IterNode {
+                parent_prefix_len,
+                key,
+                node: curr_node,
+            }],
+        }
+    }
 }
 
 impl Default for RadixTree {
@@ -292,9 +358,78 @@ impl Default for RadixTree {
     }
 }
 
+#[derive(Debug)]
+struct IterNode<'a> {
+    parent_prefix_len: usize,
+    // root does not have key
+    key: Option<u8>,
+    node: &'a Node,
+}
+
+pub struct Iter<'a> {
+    prefix: Vec<u8>,
+    nodes: Vec<IterNode<'a>>,
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(iter_node) = self.nodes.pop() {
+                let IterNode {
+                    parent_prefix_len,
+                    key,
+                    node,
+                } = iter_node;
+
+                self.prefix.truncate(parent_prefix_len);
+                if let Some(key) = key {
+                    self.prefix.push(key);
+                }
+                self.prefix.extend(node.chunk.iter());
+
+                for (child_key, child_node) in node.nodes.iter() {
+                    self.nodes.push(IterNode {
+                        parent_prefix_len: self.prefix.len(),
+                        key: Some(*child_key),
+                        node: child_node,
+                    })
+                }
+
+                if node.is_end {
+                    return Some(unsafe { String::from_utf8_unchecked(self.prefix.clone()) });
+                }
+            } else {
+                return None;
+            }
+        }
+    }
+}
+
+impl Drop for RadixTree {
+    fn drop(&mut self) {
+        let curr_node = mem::replace(
+            &mut self.root,
+            Node {
+                chunk: Vec::new(),
+                nodes: HashMap::new(),
+                is_end: false,
+            },
+        );
+
+        let mut stack: Vec<Node> = vec![curr_node];
+        while let Some(node) = stack.pop() {
+            for (_key, child_node) in node.nodes.into_iter() {
+                stack.push(child_node);
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::vec;
+    use std::{collections::HashSet, vec};
 
     use super::*;
 
@@ -309,31 +444,6 @@ mod tests {
         assert_eq!(tree.size(), 0);
         assert!(tree.is_empty());
     }
-
-    // #[test]
-    // fn test_all_ops() {
-    //     let mut tree = RadixTree::new();
-    //     let words = [
-    //         "a", "b", "c", "aa", "ab", "ac", "ba", "bb", "bc", "aaa", "aab", "aac", "bba", "bbb",
-    //         "bbc", "cca", "ccb", "ccc",
-    //     ];
-    //
-    //     for (index, word) in words.iter().enumerate() {
-    //         assert_eq!(tree.insert(word.to_string()), Ok(()));
-    //         assert_eq!(tree.size(), index + 1);
-    //         assert!(!tree.is_empty());
-    //         assert_eq!(tree.insert(word.to_string()), Err(DuplicateItemErr));
-    //         assert_eq!(tree.size(), index + 1);
-    //     }
-    //
-    //     for (index, word) in words.iter().enumerate() {
-    //         assert!(!tree.is_empty());
-    //         assert!(tree.remove(word.to_string()));
-    //         assert_eq!(tree.size(), words.len() - index - 1);
-    //         assert!(!tree.remove(word.to_string()));
-    //         assert_eq!(tree.size(), words.len() - index - 1);
-    //     }
-    // }
 
     #[test]
     fn test_empty_tree() {
@@ -373,6 +483,97 @@ mod tests {
         assert_eq!(tree, old_expected_tree);
         assert!(!tree.contains_exact(word));
         assert!(!tree.contains_prefix(word));
+        assert!(!tree.remove(""));
+    }
+
+    #[test]
+    fn test_empty_string_empty_tree() {
+        let old_expected_tree = RadixTree {
+            root: Node {
+                chunk: Vec::new(),
+                is_end: false,
+                nodes: HashMap::new(),
+            },
+            size: 0,
+        };
+        let new_expected_tree = RadixTree {
+            root: Node {
+                chunk: Vec::new(),
+                is_end: true,
+                nodes: HashMap::new(),
+            },
+            size: 1,
+        };
+
+        let mut tree = RadixTree::new();
+
+        // validate before mutate
+        assert_eq!(tree, old_expected_tree);
+        assert!(!tree.contains_exact(""));
+        assert!(!tree.contains_prefix(""));
+
+        // insert and validate
+        assert_eq!(tree.insert(""), Ok(()));
+        assert_eq!(tree, new_expected_tree);
+        assert!(tree.contains_exact(""));
+        assert!(tree.contains_prefix(""));
+
+        // remove and validate
+        assert!(tree.remove(""));
+        assert_eq!(tree, old_expected_tree);
+        assert!(!tree.contains_exact(""));
+        assert!(!tree.contains_prefix(""));
+        assert!(!tree.remove(""));
+    }
+
+    #[test]
+    fn test_empty_string_non_empty_tree() {
+        let old_expected_tree = RadixTree {
+            root: Node {
+                chunk: vec![97, 98, 99],
+                is_end: true,
+                nodes: HashMap::new(),
+            },
+            size: 1,
+        };
+        let new_expected_tree = RadixTree {
+            root: Node {
+                chunk: Vec::new(),
+                is_end: true,
+                nodes: [(
+                    97,
+                    Node {
+                        chunk: vec![98, 99],
+                        is_end: true,
+                        nodes: HashMap::new(),
+                    },
+                )]
+                .into_iter()
+                .collect(),
+            },
+            size: 2,
+        };
+
+        let mut tree = RadixTree::new();
+        assert_eq!(tree.insert("abc"), Ok(()));
+
+        // validate before mutate
+        assert_eq!(tree, old_expected_tree);
+        assert!(!tree.contains_exact(""));
+        assert!(tree.contains_prefix(""));
+
+        // insert and validate
+        assert_eq!(tree.insert(""), Ok(()));
+        assert_eq!(tree, new_expected_tree);
+        assert!(tree.contains_exact(""));
+        assert!(tree.contains_prefix(""));
+
+        // remove and validate
+        assert!(tree.remove(""));
+        assert_eq!(tree, old_expected_tree);
+        assert!(!tree.contains_exact(""));
+        assert!(tree.contains_prefix(""));
+        assert!(!tree.remove(""));
     }
 
     #[test]
@@ -433,6 +634,7 @@ mod tests {
         assert_eq!(tree, old_expected_tree);
         assert!(!tree.contains_exact(word));
         assert!(!tree.contains_prefix(word));
+        assert!(!tree.remove(""));
     }
 
     #[test]
@@ -513,6 +715,7 @@ mod tests {
         assert_eq!(tree, old_expected_tree);
         assert!(!tree.contains_exact(word));
         assert!(tree.contains_prefix(word));
+        assert!(!tree.remove(""));
     }
 
     #[test]
@@ -564,6 +767,7 @@ mod tests {
         assert_eq!(tree, old_expected_tree);
         assert!(!tree.contains_exact(word));
         assert!(tree.contains_prefix(word));
+        assert!(!tree.remove(""));
     }
 
     #[test]
@@ -654,6 +858,7 @@ mod tests {
         assert_eq!(tree, old_expected_tree);
         assert!(!tree.contains_exact(word));
         assert!(tree.contains_prefix(word));
+        assert!(!tree.remove(""));
     }
 
     #[test]
@@ -743,6 +948,7 @@ mod tests {
         assert_eq!(tree, old_expected_tree);
         assert!(!tree.contains_exact(word));
         assert!(!tree.contains_prefix(word));
+        assert!(!tree.remove(""));
     }
 
     #[test]
@@ -803,6 +1009,7 @@ mod tests {
         assert_eq!(tree, old_expected_tree);
         assert!(!tree.contains_exact(word));
         assert!(!tree.contains_prefix(word));
+        assert!(!tree.remove(""));
     }
 
     #[test]
@@ -892,6 +1099,7 @@ mod tests {
         assert_eq!(tree, old_expected_tree);
         assert!(!tree.contains_exact(word));
         assert!(tree.contains_prefix(word));
+        assert!(!tree.remove(""));
     }
 
     #[test]
@@ -1011,6 +1219,7 @@ mod tests {
         assert_eq!(tree, old_expected_tree);
         assert!(!tree.contains_exact(word));
         assert!(tree.contains_prefix(word));
+        assert!(!tree.remove(""));
     }
 
     #[test]
@@ -1085,5 +1294,217 @@ mod tests {
         assert!(!tree.remove("abcf"));
         // false if root chunk and word mismatches
         assert!(!tree.remove("abcfh"));
+    }
+
+    fn get_unicode_words() -> Vec<String> {
+        let words = vec![
+            "你好",
+            "🥢",
+            "Hi, 🥢",
+            "hello",
+            "我好嗎",
+            "🍣",
+            "すしや",
+            "hello, world",
+            "你好，世界",
+            "🥷",
+            "すし",
+            "你很好",
+            "Hello,你很好",
+            "hell",
+            "🍜",
+            "すしづめ",
+            "我好吗",
+            "🥷🍜🐔🍣",
+            "すしだね",
+            "🐔🍣",
+            "hello!",
+            "すしめし",
+            "hey! すしめし",
+            "🥷🍜",
+            "你好吗",
+            "🥢🍜🍣",
+            "Hey",
+            "🥢🍣🍜",
+            "すしをにぎる",
+            "🐔",
+            "你好嗎",
+        ];
+
+        words.iter().map(|word| word.to_string()).collect()
+    }
+
+    #[test]
+    fn test_many_insert_removes() {
+        let mut tree = RadixTree::new();
+        let words = get_unicode_words();
+
+        for (index, word) in words.iter().enumerate() {
+            assert_eq!(tree.insert(word), Ok(()));
+            assert_eq!(tree.size(), index + 1);
+            assert!(!tree.is_empty());
+            assert_eq!(tree.insert(word), Err(DuplicateItemErr));
+            assert_eq!(tree.size(), index + 1);
+        }
+
+        for (index, word) in words.iter().enumerate() {
+            assert!(!tree.is_empty());
+            assert!(tree.remove(word));
+            assert_eq!(tree.size(), words.len() - index - 1);
+            assert!(!tree.remove(word));
+            assert_eq!(tree.size(), words.len() - index - 1);
+        }
+    }
+
+    fn get_prefixes(word: &str) -> Vec<String> {
+        let mut chars = word.chars();
+        let mut prefixes = vec![chars.as_str().to_string()];
+        while chars.next_back().is_some() {
+            prefixes.push(chars.as_str().to_string());
+        }
+        prefixes
+    }
+
+    #[test]
+    fn test_contains_prefix() {
+        let mut tree = RadixTree::new();
+
+        let mut words = get_unicode_words();
+        let mut inserted_words: Vec<String> = Vec::new();
+
+        while let Some(word) = words.pop() {
+            assert_eq!(tree.insert(&word), Ok(()));
+            inserted_words.push(word);
+
+            let inserted_prefixes: Vec<String> = inserted_words
+                .iter()
+                .map(|str| str.as_str())
+                .flat_map(get_prefixes)
+                .collect();
+            let inserted_prefixes_set: HashSet<String> = HashSet::from_iter(inserted_prefixes);
+            let prefixes_to_insert: Vec<String> = words
+                .iter()
+                .map(|str| str.as_str())
+                .flat_map(get_prefixes)
+                .filter(|word| !inserted_prefixes_set.contains(word))
+                .collect();
+            let prefixes_to_insert_set: HashSet<String> = HashSet::from_iter(prefixes_to_insert);
+
+            for prefix in inserted_prefixes_set.iter() {
+                assert!(tree.contains_prefix(prefix));
+            }
+
+            for prefix in prefixes_to_insert_set.iter() {
+                assert!(!tree.contains_prefix(prefix));
+            }
+        }
+
+        let mut words = get_unicode_words();
+        let mut removed_words: Vec<String> = Vec::new();
+
+        while let Some(word) = words.pop() {
+            assert!(tree.remove(&word));
+            removed_words.push(word);
+
+            let prefixes: Vec<String> = words
+                .iter()
+                .map(|str| str.as_str())
+                .flat_map(get_prefixes)
+                .collect();
+            let prefixes_set: HashSet<String> = HashSet::from_iter(prefixes);
+            let removed_prefixes: Vec<String> = removed_words
+                .iter()
+                .map(|str| str.as_str())
+                .flat_map(get_prefixes)
+                .filter(|word| !prefixes_set.contains(word))
+                .collect();
+            let removed_prefixes_set: HashSet<String> = HashSet::from_iter(removed_prefixes);
+
+            for prefix in prefixes_set.iter() {
+                assert!(tree.contains_prefix(prefix));
+            }
+
+            for prefix in removed_prefixes_set.iter() {
+                assert!(!tree.contains_prefix(prefix));
+            }
+        }
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut tree = RadixTree::new();
+        assert_eq!(tree.iter().next(), None);
+
+        let mut words = get_unicode_words();
+        let mut inserted_words: Vec<String> = Vec::new();
+
+        while let Some(word) = words.pop() {
+            assert_eq!(tree.insert(&word), Ok(()));
+            inserted_words.push(word);
+
+            for word in tree.iter() {
+                assert!(inserted_words.contains(&word));
+                assert!(!words.contains(&word));
+            }
+        }
+
+        let mut words = get_unicode_words();
+        let mut removed_words: Vec<String> = Vec::new();
+
+        while let Some(word) = words.pop() {
+            assert!(tree.remove(&word));
+            removed_words.push(word);
+
+            for word in tree.iter() {
+                assert!(words.contains(&word));
+                assert!(!removed_words.contains(&word));
+            }
+        }
+    }
+
+    #[test]
+    fn test_iter_prefix() {
+        let mut tree = RadixTree::new();
+
+        let mut words = get_unicode_words();
+        let mut inserted_words: Vec<String> = Vec::new();
+
+        for _i in 0..25 {
+            let word = words.pop().unwrap();
+            assert_eq!(tree.insert(&word), Ok(()));
+            inserted_words.push(word);
+        }
+
+        let inserted_prefixes: Vec<String> = inserted_words
+            .iter()
+            .map(|str| str.as_str())
+            .flat_map(get_prefixes)
+            .collect();
+        let inserted_prefixes_set: HashSet<String> = HashSet::from_iter(inserted_prefixes);
+        let non_inserted_prefixes: Vec<String> = words
+            .iter()
+            .map(|str| str.as_str())
+            .flat_map(get_prefixes)
+            .filter(|word| !inserted_prefixes_set.contains(word))
+            .collect();
+        let non_inserted_prefixes_set: HashSet<String> = HashSet::from_iter(non_inserted_prefixes);
+
+        for prefix in inserted_prefixes_set {
+            let words_with_prefix: Vec<String> = inserted_words
+                .iter()
+                .filter(|word| word.starts_with(prefix.as_str()))
+                .cloned()
+                .collect();
+            let tree_iter_prefix_words: Vec<String> = tree.iter_prefix(&prefix).collect();
+            assert_eq!(
+                HashSet::from_iter(words_with_prefix) as HashSet<String>,
+                HashSet::from_iter(tree_iter_prefix_words) as HashSet<String>
+            );
+        }
+
+        for prefix in non_inserted_prefixes_set {
+            assert_eq!(tree.iter_prefix(&prefix).next(), None);
+        }
+        assert_eq!(tree.iter_prefix("hello, world!!!").next(), None);
     }
 }
